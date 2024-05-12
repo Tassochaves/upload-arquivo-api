c9198e46e7ddc84585351e10c8c048ddf47d33ef
MeusClientes <meusclientes.prod@gmail.com>
2024-05-12 15:31:04 -0300

    File Upload Example

---
+++ src/test/java/com/dev/uploadarquivosapi/UploadArquivosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.uploadarquivosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadArquivosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
